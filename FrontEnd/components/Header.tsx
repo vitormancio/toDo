3b377e710dbf33cd0cd80735759ad3e22de46b01
vitordev <vitor.mancio@gmail.com>
2023-03-10 01:47:37 -0300

    [feat] develop first Navbar

--- FrontEnd/components/Header.tsx
+++ FrontEnd/components/Header.tsx
@@ -1,12 +1,27 @@
-import { Navbar, Text } from "@nextui-org/react"
-import { BrandLogo } from "./BrandLogo"
+import { Navbar, Text, Button } from "@nextui-org/react";
+import { BrandLogo } from "./BrandLogo";
+import { CiSettings } from "react-icons/ci";
 
 export const Header = () => {
     return (
-        <Navbar>
+        <Navbar variant={"static"}>
             <Navbar.Brand>
                 <BrandLogo />
             </Navbar.Brand>
+            <Navbar.Content>
+                <Navbar.Item>
+                    <Button color={'success'} bordered >
+                        <Text weight={'extrabold'}>Login</Text>
+                    </Button>
+                </Navbar.Item>
+            </Navbar.Content>
+            <Navbar.Content>
+                <Navbar.Item>
+                    <Button color={"primary"} rounded bordered auto size={'md'}>
+                        <CiSettings size={28}/>
+                    </Button>
+                </Navbar.Item>
+            </Navbar.Content>
         </Navbar> 
     )
 }

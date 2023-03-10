0029730d124520060a40904db68cba4323c03953
vitordev <vitor.mancio@gmail.com>
2023-03-10 01:31:48 -0300

    [feat]createTheme Dark from NexUi lib and set on all website

--- FrontEnd/src/pages/_app.tsx
+++ FrontEnd/src/pages/_app.tsx
@@ -1,10 +1,20 @@
-import { NextUIProvider } from '@nextui-org/react'
+import { cyanDark, NextUIProvider } from '@nextui-org/react'
 import { AppProps } from 'next/app'
 import { Header } from '../../components/Header'
 
+import { createTheme } from "@nextui-org/react"
+
+const darkTheme = createTheme({
+  type: 'dark',
+  theme: {
+    colors: {},
+  }
+});
+
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <NextUIProvider>
+    <NextUIProvider theme={darkTheme} >
       <Header/>
       <Component {...pageProps} />
     </NextUIProvider>

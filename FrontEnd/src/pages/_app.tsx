db0b6d2ba01082066be948bf492db83d59db16e6
vitordev <vitor.mancio@gmail.com>
2023-03-10 01:13:54 -0300

    [feat] import navbar on _app.tsx next file

--- FrontEnd/src/pages/_app.tsx
+++ FrontEnd/src/pages/_app.tsx
@@ -1,9 +1,11 @@
 import { NextUIProvider } from '@nextui-org/react'
 import { AppProps } from 'next/app'
+import { Header } from '../../components/Header'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <NextUIProvider>
+      <Header/>
       <Component {...pageProps} />
     </NextUIProvider>
   )

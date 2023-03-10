b36db9f5f95e236ca81c62b330693a505c7c3edf
vitordev <vitor.mancio@gmail.com>
2023-03-10 03:19:51 -0300

    [feat] create new page and create/import Css from new folder

---
+++ FrontEnd/src/pages/Login/index.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import { LoginContainer, LoginContent } from '../../styles/pages/Login/index'
+
+export default function LoginPage () {
+    return (
+        <>
+        <Head>
+
+        </Head>
+
+        <LoginContainer>
+            
+        </LoginContainer>
+        </>
+    )
+}
---
+++ FrontEnd/src/styles/pages/Login/index.ts
@@ -0,0 +1,12 @@
+import { styled } from "@nextui-org/react";
+
+export const LoginContainer = styled('div', {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+})
+
+export const LoginContent = styled('div', {
+    
+})
